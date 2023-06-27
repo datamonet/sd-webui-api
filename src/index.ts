29492d7aed2e982daa58a661542320b217ade694
faye <faye_1225@163.com>
2023-06-27 13:13:13 +0800

    change:index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+export * from "./lib/ControlNetApi";
+export * from "./lib/ControlNetUnit";
+export * from "./lib/StableDiffusionApi";
+export * from "./lib/StableDiffusionResult";
+export * from "./types";
+export * from "./utils/base64";
+
+export { StableDiffusionApi as default } from "./lib/StableDiffusionApi";
