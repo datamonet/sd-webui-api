8c435da366c0fa83b9a0ab802c06215bf11442ce
faye <faye_1225@163.com>
2023-06-27 16:27:55 +0800

    del:lib

--- lib/utils/base64.d.ts
+++
@@ -1,9 +0,0 @@
-import { type Sharp } from "sharp";
-/**
- * Converts an image buffer to base64
- *
- * @param {Buffer} image image buffer to convert to base64
- * @param {boolean} raw if true, returns the raw base64 string, if false, returns a data url with the base64 string
- * @returns {Promise<string>} base64 encoded image
- */
-export declare function toBase64(image: Sharp, raw?: boolean): Promise<string>;
